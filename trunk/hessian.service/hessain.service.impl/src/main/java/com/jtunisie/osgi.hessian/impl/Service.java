56c854e84c9302b30bec3ea194c79e30dfa66dbf
ouertani <ouertani@83cc9468-e552-0410-8a4c-37f2070202ac>
2008-07-26 04:44:23 +0000

    [SO] add hessain extender

--- trunk/hessian.service/hessain.service.impl/src/main/java/com/jtunisie/osgi.hessian/impl/Service.java
+++ trunk/hessian.service/hessain.service.impl/src/main/java/com/jtunisie/osgi.hessian/impl/Service.java
@@ -5,16 +5,16 @@ import com.jtunisie.osgi.hessian.IService;
 
 import com.jtunisie.osgi.hessian.exceptions.ServiceException;
 
-
-
-
 /**
  *
  * @author Slim OUERTANI
  */
 public class Service extends HessianServlet implements IService {
 
-   
+    public void publish() {
+        this.setAPIClass(IService.class);
+    }
+
     @Override
     public String execute() throws ServiceException {
         return "jTunisie";
